3736feea6e7ca563bad875edbac0a034b87b0bee
offcn-java <myqq@qq.com>
2021-11-17 19:29:10 +0800

    首次提交

---
+++ src/JVM.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+/**
+ * @author W.Wen
+ * @version : V1.0
+ * @className: JVM
+ * @description: 首次提交
+ * @date 2021-11-17
+ */
+public class JVM {
+    public static void main(String[] args) {
+        System.out.println("首次提交！");
+    }
+}
